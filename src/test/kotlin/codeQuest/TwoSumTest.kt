cfc9b8090b55b13afd3a089bd45b4f156155faf1
jeanniehjan@gmail.com <jeanniehjan@gmail.com>
2024-05-11 10:59:04 +0100

    implemented TwoSum

--- src/test/kotlin/codeQuest/TwoSumTest.kt
+++ src/test/kotlin/codeQuest/TwoSumTest.kt
@@ -16,7 +16,82 @@ class TwoSumTest {
     }
 
     @Test
-    fun twoSum() {
+    fun testTwoSum_1() {
+        val nums = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        val expected = intArrayOf(0, 1)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
 
+    @Test
+    fun testTwoSum_2() {
+        val nums = intArrayOf(3, 2, 4)
+        val target = 6
+        val expected = intArrayOf(1, 2)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_3() {
+        val nums = intArrayOf(3, 3)
+        val target = 6
+        val expected = intArrayOf(0, 1)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_4() {
+        val nums = intArrayOf(0, 4, 3, 0)
+        val target = 0
+        val expected = intArrayOf(0, 3)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_5() {
+        val nums = intArrayOf(-1, -2, -3, -4, -5)
+        val target = -8
+        val expected = intArrayOf(2, 4)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_6() {
+        val nums = intArrayOf(1, 2, 3, 4, 5)
+        val target = 10
+        val expected = intArrayOf(3, 4)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_7() {
+        val nums = intArrayOf(1, 2, 3, 4, 5)
+        val target = 2
+        val expected = intArrayOf(0, 1)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_8() {
+        val nums = intArrayOf(1, 2)
+        val target = 3
+        val expected = intArrayOf(0, 1)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_9() {
+        val nums = intArrayOf(1)
+        val target = 1
+        val expected = intArrayOf(0)
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_10() {
+        val nums = intArrayOf(1, 2, 3, 4, 5)
+        val target = 20
+        val expected = intArrayOf()
+        assertArrayEquals(expected, twoSum.twoSum(nums, target))
     }
 }
