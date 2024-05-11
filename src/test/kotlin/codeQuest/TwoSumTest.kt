4f69358e9d8333222b0970df0ac90319abb20524
jeanniehjan@gmail.com <jeanniehjan@gmail.com>
2024-05-11 10:57:35 +0100

    implemented TwoSum

---
+++ src/test/kotlin/codeQuest/TwoSumTest.kt
@@ -0,0 +1,22 @@
+package codeQuest
+
+import org.example.codeQuest.TwoSum
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class TwoSumTest {
+
+    private lateinit var twoSum: TwoSum
+
+    @BeforeEach
+    fun setUp() {
+        twoSum = TwoSum()
+    }
+
+    @Test
+    fun twoSum() {
+
+    }
+}
