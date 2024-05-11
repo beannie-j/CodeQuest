8f2b552a4a56798a1a2eb102713fce2df3a60c31
jeanniehjan@gmail.com <jeanniehjan@gmail.com>
2024-05-11 13:38:12 +0100

    implemented MergeTwoSortedLists

--- src/main/kotlin/codeQuest/MergeTwoSortedLists.kt
+++ src/main/kotlin/codeQuest/MergeTwoSortedLists.kt
@@ -3,8 +3,33 @@ package org.example.codeQuest
 class MergeTwoSortedLists {
     companion object {
         fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+            val mergedList = ListNode()
+            var mergedCurrent = mergedList
 
-            return ListNode(0)
+            var firstCurrent = list1
+            var secondCurrent = list2
+
+            while (firstCurrent != null && secondCurrent != null) {
+                val firstNodeValue = firstCurrent.`val`
+                val secondNodeValue = secondCurrent.`val`
+
+                println("First node value: $firstNodeValue")
+                println("Second node value: $secondNodeValue")
+
+                if (firstNodeValue <= secondNodeValue) {
+                    mergedCurrent.next = firstCurrent
+                    firstCurrent = firstCurrent.next
+                } else {
+                    mergedCurrent.next = secondCurrent
+                    secondCurrent = secondCurrent.next
+                }
+
+                mergedCurrent = mergedCurrent.next ?: break
+            }
+
+            mergedCurrent.next = firstCurrent ?: secondCurrent
+
+            return mergedList.next
         }
     }
 }
